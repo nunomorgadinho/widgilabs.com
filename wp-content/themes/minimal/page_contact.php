44eff0a438c71805cb6bd827cb9fce388089566a
Nuno Morgadinho <nuno.morgadinho@gmail.com>
2012-06-09 01:01:01 +0100

    add map

--- wp-content/themes/minimal/page_contact.php
+++ wp-content/themes/minimal/page_contact.php
@@ -7,9 +7,9 @@ get_header(); ?>
 <!-- column container -->
 <div class="clearfix">
 	
-	
-	<h2><?php the_title(); ?></h2>
-	<?php include_once("subnav.php"); ?>
+	<iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=R.+Rodrigues+Faria+103,+1300-501+Lisboa,+Portugal&amp;aq=&amp;sll=38.726067,-9.164109&amp;sspn=0.081156,0.1478&amp;t=m&amp;ie=UTF8&amp;hq=&amp;hnear=R.+Rodrigues+de+Faria+103,+Alc%C3%A2ntara,+1300+Lisboa,+Portugal&amp;ll=38.710965,-9.178905&amp;spn=0.023441,0.036478&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=R.+Rodrigues+Faria+103,+1300-501+Lisboa,+Portugal&amp;aq=&amp;sll=38.726067,-9.164109&amp;sspn=0.081156,0.1478&amp;t=m&amp;ie=UTF8&amp;hq=&amp;hnear=R.+Rodrigues+de+Faria+103,+Alc%C3%A2ntara,+1300+Lisboa,+Portugal&amp;ll=38.710965,-9.178905&amp;spn=0.023441,0.036478&amp;z=14&amp;iwloc=A" style="color:#0000FF;text-align:left"></a></small>
+
+
 	
 	<div class="column column600">
     	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
